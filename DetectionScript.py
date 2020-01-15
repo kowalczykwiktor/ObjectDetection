64a75a4e1af76133287593ac83a09a6c7db79d90
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2020-01-15 13:13:57 +0100

    installLibraryDedicatedToDetection

---
+++ DetectionScript.py
@@ -0,0 +1,11 @@
+pip3 install tensorflow
+pip3 install numpy
+pip3 install scipy
+pip3 install opencv-python
+pip3 install pillow
+pip3 install matplotlib
+pip3 install h5py
+pip3 install keras
+pip3 install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.1/imageai-2.0.1-py3-none-any.whl
+
+
