a299d1ae091c97143dbca1805714aed34598a186
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2020-01-16 14:50:16 +0100

    DetectionSection

--- DetectionScript.py
+++ DetectionScript.py
@@ -15,3 +15,10 @@ import os
 
 execution_path = os.getcwd()
 
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "ObjectDetection.jpg"))
+
+
