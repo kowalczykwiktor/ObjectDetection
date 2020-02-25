b276e03b9acb431c486724aad66c85ee1f0a472d
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2020-02-25 12:30:23 +0100

    Download_RetinaNetFile_Description

--- DetectionScript.py
+++ DetectionScript.py
@@ -1,14 +1,19 @@
 # python3 -m pip install --upgrade pip
 
-pip3 install tensorflow
-pip3 install numpy
-pip3 install scipy
-pip3 install opencv-python
-pip3 install pillow
-pip3 install matplotlib
-pip3 install h5py
-pip3 install keras
-pip3 install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.1/imageai-2.0.1-py3-none-any.whl
+# pip3 install tensorflow
+# pip3 install numpy
+# pip3 install scipy
+# pip3 install opencv-python
+# pip3 install pillow
+# pip3 install matplotlib
+# pip3 install h5py
+# pip3 install keras
+# pip3 install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.1/imageai-2.0.1-py3-none-any.whl
+
+'''
+DOWNLOAD RetinaNet from here:
+https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
+'''
 
 from imageai.Detection import ObjectDetection
 import os
